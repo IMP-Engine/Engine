1108544c416a3e8ee1223a9fa5b432742043f1bd
Patrick Andersson <andersson.patrick95@gmail.com>
2017-02-12 16:29:12 +0100

    Added simple draft of physics simulation loop

---
+++ src/physics.cpp
@@ -0,0 +1,51 @@
+#include "physics.h"
+
+
+namespace physics {
+
+void simulate(std::vector<Particle*>* particles/*, std::vector<Constraint*>* constraints */, float dt)
+{
+	std::vector<float3> pPos(particles->size());
+	// Based on 2007 PBD, NOT Unified Framework 
+
+	// For all particles i
+	// Apply forces			v_i = v_i + dt * f_ext(x_i)
+	// Damp velocities		-- Skip for now --
+	// Predict position		x_i^* = x_i + dt * v_i
+
+	for (std::vector<float3>::size_type i = 0; i != particles->size(); i++) {
+		//APPLY GRAVITY HERE
+		pPos[i] = (*particles)[i]->pos + dt * (*particles)[i]->velocity;
+	}
+
+
+
+	// For all particles i
+	// Generate collision constraints
+	/* Add constraints from collisions to already given list of constraints */
+
+
+
+	// loop until satisfied
+	// Project constraints (C1,...,CM, x_1^*,...x_n^*)
+	/* Stationary iterative linear solver */
+
+
+
+	// For all particles i
+	// v_i = (x_i^* - x_i) / dt
+	// x_i = x_i^*
+
+	for (std::vector<float3>::size_type i = 0; i != particles->size(); i++) {
+	{
+		(*particles)[i]->velocity = (pPos[i] - (*particles)[i]->pos) / dt;
+		(*particles)[i]->pos = pPos[i];
+	}
+
+
+		// Update velocities according to friction and restituition coefficients
+		/* Skip this for now */
+}
+
+			
+}
