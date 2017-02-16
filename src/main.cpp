0788cfc1805587365578942ff3de2af43176798d
bondi <pontus_eriksson@live.com>
2017-02-16 12:29:13 +0100

    Now draws gui as intended again.

--- src/main.cpp
+++ src/main.cpp
@@ -322,6 +322,8 @@ void display() {
 	glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE, &size);
 	glDrawElements(GL_TRIANGLES, size / sizeof(GLushort), GL_UNSIGNED_SHORT, 0); //sizeof(GLushort),
 
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
 	//glDrawArrays(GL_TRIANGLES, 0, 3);
 	//glDrawArrays(GL_TRIANGLES, 3, 3);
 
