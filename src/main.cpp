438e205b924c8e153a660e36d67ea9f9ca9cf3a5
bondi <pontus_eriksson@live.com>
2017-02-16 14:15:58 +0100

    Removed a lot of unused code.

--- src/main.cpp
+++ src/main.cpp
@@ -31,12 +31,12 @@ using namespace glm;
 // Global variables
 GLFWwindow* window;
 GLint mvp_location, vpos_location, vcol_location;
-GLuint cube_ibo;
+GLuint cube_ibo; // IndicesBufferObject
 
 // Shaders
 GLuint simpleShader;
 
-float vertices[] = {
+float cubeVertices[] = {
 	-10.0f, -10.0f,  10.0f,
 	 10.0f, -10.0f,  10.0f,
 	 10.0f,  10.0f,  10.0f,
@@ -47,92 +47,6 @@ float vertices[] = {
 	 10.0f,  10.0f, -10.0f,
 	-10.0f,  10.0f, -10.0f,
 };
-
-// Framebuffer
-//struct FboInfo;
-//std::vector<FboInfo> fboList;
-//
-//struct FboInfo {
-//	GLuint framebufferId;
-//	GLuint colorTextureTarget;
-//	GLuint depthBuffer;
-//	int width;
-//	int height;
-//	bool isComplete;
-//
-//	FboInfo(int w, int h) {
-//		isComplete = false;
-//		width = w;
-//		height = h;
-//
-//		// Generate two textures and set filter parameters (no storage allocated yet)
-//		glGenTextures(1, &colorTextureTarget);
-//		glBindTexture(GL_TEXTURE_2D, colorTextureTarget);
-//		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-//		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//
-//		glGenTextures(1, &depthBuffer);
-//		glBindTexture(GL_TEXTURE_2D, depthBuffer);
-//		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-//		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//
-//		// allocate storage for textures
-//		resize(width, height);
-//
-//		// Generate and bind framebuffer
-//		// check if framebuffer is complete
-//		glGenFramebuffers(1, &framebufferId);
-//		glBindFramebuffer(GL_FRAMEBUFFER, framebufferId);
-//
-//		// bind the texture as color attachment 0 (to the currently bound framebuffer)
-//		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorTextureTarget, 0);
-//		glDrawBuffer(GL_COLOR_ATTACHMENT0);
-//
-//		// bind the texture as depth attachment (to the currently bound framebuffer)
-//		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthBuffer, 0);
-//
-//		isComplete = checkFramebufferComplete();
-//
-//		// bind default framebuffer, just in case.
-//		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-//	}
-//
-//	// if no resolution provided
-//	FboInfo() : isComplete(false)
-//		, framebufferId(UINT32_MAX)
-//		, colorTextureTarget(UINT32_MAX)
-//		, depthBuffer(UINT32_MAX)
-//		, width(0)
-//		, height(0)
-//	{};
-//
-//	void resize(int w, int h) {
-//		width = w;
-//		height = h;
-//		// Allocate a texture
-//		glBindTexture(GL_TEXTURE_2D, colorTextureTarget);
-//		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-//
-//		// generate a depth texture
-//		glBindTexture(GL_TEXTURE_2D, depthBuffer);
-//		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT32, width, height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
-//	}
-//
-//	bool checkFramebufferComplete(void) {
-//		// Check that our FBO is correctly set up, this can fail if we have
-//		// incompatible formats in a buffer, or for example if we specify an
-//		// invalid drawbuffer, among things.
-//		glBindFramebuffer(GL_FRAMEBUFFER, framebufferId);
-//		GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-//		if (status != GL_FRAMEBUFFER_COMPLETE) {
-//			//glHelper::fatal_error("Framebuffer not complete");
-//			// Error
-//		}
-//
-//		return (status == GL_FRAMEBUFFER_COMPLETE);
-//	}
-//};
-// ************************ End Framebuffer *************************
 
 static void error_callback(int error, const char* description) {
     fprintf(stderr, "Error: %s\n", description);
@@ -189,21 +103,11 @@ void initGL() {
 
 	// NOTE: OpenGL error checks have been omitted for brevity
 
-	//glEnable(GL_DEPTH_TEST);
-	//glEnable(GL_CULL_FACE);
-
 	// Shader setup
 	simpleShader = glHelper::loadShader(VERT_SHADER_PATH, FRAG_SHADER_PATH);
 	mvp_location = glGetUniformLocation(simpleShader, "MVP");
 	vpos_location = glGetAttribLocation(simpleShader, "vPos");
 	vcol_location = glGetAttribLocation(simpleShader, "vCol");
-
-	// Framebuffer setup
-	/*int w, h;
-	glfwGetWindowSize(window, &w, &h);
-	const int numFbos = 5;
-	for (int i = 0; i < numFbos; i++)
-		fboList.push_back(FboInfo(w, h));*/
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		printf("Failed to initialize OpenGL context");
@@ -215,15 +119,10 @@ void initGL() {
 
 	glGenBuffers(1, &vertex_buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	//glEnableVertexAttribArray(vpos_location);
-	//glVertexAttribPointer(vpos_location, 3, GL_FLOAT, GL_FALSE,
-	//	/*sizeof(float) * 3, (void*)0);*/
-	//	0, (void*)0);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
 
 	// Cube setup
-	GLushort indices[] = {
+	GLushort cubeIndices[] = {
 		// front
 		0, 1, 2,
 		2, 3, 0,
@@ -246,12 +145,10 @@ void initGL() {
 
 	glGenBuffers(1, &cube_ibo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cube_ibo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndices), cubeIndices, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(vpos_location);
-	glVertexAttribPointer(vpos_location, 3, GL_FLOAT, GL_FALSE,
-		/*sizeof(float) * 3, (void*)0);*/
-		0, (void*)0);
+	glVertexAttribPointer(vpos_location, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
 
 	glDisable(GL_CULL_FACE);
 	glEnable(GL_DEPTH_TEST);
@@ -260,7 +157,7 @@ void initGL() {
 void display() {
 	float ratio;
 	int width, height;
-	mat4 v, p, mvp, mv;
+	mat4 viewMatrix, modelViewProjectionMatrix, modelViewMatrix;
 
 	glfwGetFramebufferSize(window, &width, &height);
 	ratio = width / (float)height;
@@ -268,24 +165,22 @@ void display() {
 	glViewport(0, 0, width, height);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	mat4 m(1.0f); // Identity matrix
+	mat4 modelMatrix(1.0f); // Identity matrix
 
-	//m = rotate((float)glfwGetTime(), vec3(0, 0, 1)) * m;
-	v = lookAt(vec3(20, 4, 40), vec3(0), vec3(0, 1, 0));
+	viewMatrix = lookAt(vec3(20, 4, 40), vec3(0), vec3(0, 1, 0));
 
 	// Set up a projection matrix
 	float fovy = radians(45.0f);
 	float nearPlane = 0.01f;
 	float farPlane = 300.0f;
 
-	p = perspective(fovy, ratio, nearPlane, farPlane); //ortho(-1.f, 1.f, 1.f, -1.f);
-	mv = v * m;
-	mvp = p * mv;
+	modelViewMatrix = viewMatrix * modelMatrix;
+	modelViewProjectionMatrix = perspective(fovy, ratio, nearPlane, farPlane) * modelViewMatrix;
 
 	// Send uniforms to shader
 	glUseProgram(simpleShader);
-	glUniformMatrix4fv(glGetUniformLocation(simpleShader, "MVP"), 1, false, &mvp[0].x);
-	glUniformMatrix4fv(glGetUniformLocation(simpleShader, "MV"), 1, false, &mv[0].x);
+	glUniformMatrix4fv(glGetUniformLocation(simpleShader, "MVP"), 1, false, &modelViewProjectionMatrix[0].x);
+	glUniformMatrix4fv(glGetUniformLocation(simpleShader, "MV"), 1, false, &modelViewMatrix[0].x);
 
 	// Draw
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -294,13 +189,9 @@ void display() {
 	glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE, &size);
 	glDrawElements(GL_TRIANGLES, size / sizeof(GLushort), GL_UNSIGNED_SHORT, 0); //sizeof(GLushort),
 
+	// GUI
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-	//glDrawArrays(GL_TRIANGLES, 0, 3);
-	//glDrawArrays(GL_TRIANGLES, 3, 3);
-
-
-  ImGui::Render();
+	ImGui::Render();
 
 	glfwSwapBuffers(window);
 }
@@ -310,9 +201,6 @@ int main(void) {
 
 	printf("OpenGL %d.%d\n", GLVersion.major, GLVersion.minor);
 
-	//if (GLAD_GL_EXT_framebuffer_multisample) {
-	//	/* GL_EXT_framebuffer_multisample is supported */
-	//}
 	if (GLAD_GL_VERSION_4_3) {
 		/* We support at least OpenGL version 4.3 */
 	}
@@ -328,15 +216,12 @@ int main(void) {
 	config.phase = 1;
 	config.num_particles = vec3(3,3,3);
 
-	Box *box = make_box(&config);
-
-    
+	Box *box = make_box(&config);    
 
     while (!glfwWindowShouldClose(window))
     {
 		glfwPollEvents();
 		ImGui_ImplGlfwGL3_NewFrame();
-
 
 		ImVec4 clear_color = ImColor(164, 164, 164);
 
