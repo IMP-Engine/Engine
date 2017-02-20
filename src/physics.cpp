2c3b26b7661d2b1c4a718cfb5e4e932ded0c4689
Patrick Andersson <andersson.patrick95@gmail.com>
2017-02-20 16:39:55 +0100

    First draft of GS solver

--- src/physics.cpp
+++ src/physics.cpp
@@ -1,9 +1,10 @@
 #include "physics.h"
 #include <glm/vec3.hpp>
+#include "Constraint.h"
 
 namespace physics {
 
-void simulate(std::vector<Particle*>* particles/*, std::vector<Constraint*>* constraints */, float dt)
+void simulate(std::vector<Particle*>* particles, std::vector<Constraint*>* constraints , float dt)
 {
 	std::vector<glm::vec3> pPos(particles->size());
 	// Based on 2007 PBD, NOT Unified Framework 
@@ -26,10 +27,23 @@ void simulate(std::vector<Particle*>* particles/*, std::vector<Constraint*>* con
 
 
 
-	// loop until satisfied
-	// Project constraints (C1,...,CM, x_1^*,...x_n^*)
+
 	/* Stationary iterative linear solver */
 
+	for (size_t i = 0; i < 10; i++)
+	{
+		for (Constraint* c : *constraints)
+		{
+			if (c->evaluate())
+			{ 
+				for (std::vector<Particle*>::iterator p = c->particles.begin(); p != c->particles.end(); p++)
+				{
+					// delta p_i = w_i * s * grad_{p_i} C(p) * stiffness correction 
+					(*p)->pos -= (*p)->invmass * c->evaluateScaleFactor() * c->evaluateGradient(p) * (1 - pow(1 - c->stiffness, 1/(float)i));
+				}
+			}
+		}
+	}
 
 
 	// For all particles i
