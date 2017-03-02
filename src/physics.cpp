7544281992c9b3a3522e4725d74323b5e26e1739
bondi <pontus_eriksson@live.com>
2017-03-02 15:04:23 +0100

    Minor optimization: Merged two pair of for-loops.

--- src/physics.cpp
+++ src/physics.cpp
@@ -9,29 +9,29 @@ void simulate(std::vector<Particle>* particles, std::vector<Constraint*>* constr
 {
 	/* Based on 2007 PBD, NOT Unified Framework */
 
-	/*
-	* For all particles i
-	* Apply forces			v_i = v_i + dt * f_ext(x_i)
-	* Damp velocities		-- Skip for now -- TODO --
-	* Predict position		x_i^* = x_i + dt * v_i
-	*/
 	const float GRAVITY = 4.0f;
 	for (std::vector<glm::vec3>::size_type i = 0; i != particles->size(); i++) {
+		/*
+		* For all particles i
+		* Apply forces			v_i = v_i + dt * f_ext(x_i)
+		* Damp velocities		-- Skip for now -- TODO --
+		* Predict position		x_i^* = x_i + dt * v_i
+		*/
 		(*particles)[i].velocity = (*particles)[i].velocity - glm::vec3(0.f, dt * GRAVITY, 0.f) * (*particles)[i].invmass; // Gravity
 		(*particles)[i].pPos = (*particles)[i].pos + dt * (*particles)[i].velocity; // symplectic Euler 
-	}
+		// ******************************************************************************************************************
 
-	// For all particles i
-	// Generate collision constraints
-	/* 
-	* Add constraints from collisions to already given list of constraints 
-	*/
-	// TODO
-	// Temporary collision check and handler for some plane
-	for (std::vector<glm::vec3>::size_type i = 0; i != particles->size(); i++) {
+		// For all particles i
+		// Generate collision constraints
+		/*
+		* Add constraints from collisions to already given list of constraints
+		*/
+		// TODO
+		// Temporary collision check and handler for some plane
 		if ((*particles)[i].pPos.y < -10) {
 			(*particles)[i].pPos.y = -10;
 		}
+		// ******************************************************************************************************************
 	}
 
 	/* 
@@ -52,24 +52,24 @@ void simulate(std::vector<Particle>* particles, std::vector<Constraint*>* constr
 		}
 	}
 
-	/* 
-	 * For all particles i
-	 * v_i = (x_i^* - x_i) / dt
-	 * x_i = x_i^* 
-	 */
+	
 	for (std::vector<glm::vec3>::size_type i = 0; i != particles->size(); i++) 
 	{
+		/*
+		* For all particles i
+		* v_i = (x_i^* - x_i) / dt
+		* x_i = x_i^*
+		*/
 		(*particles)[i].velocity = ((*particles)[i].pPos - (*particles)[i].pos) / dt;
 		// rough attempt at particle sleeping implementation in order to make particles stay in one place - most likely needs proper friction to work
 		if (glm::length((*particles)[i].pos - (*particles)[i].pPos) > 0.01)
 			(*particles)[i].pos = (*particles)[i].pPos;
-	}
-	
-	/* 
-	 * Update velocities according to friction and restituition coefficients 
-	 */
-	// Naive friction implementation
-	for (std::vector<glm::vec3>::size_type i = 0; i != particles->size(); i++) {
+		// ***************************************************************************************************************************
+
+		/*
+		* Update velocities according to friction and restituition coefficients
+		*/
+		// Naive friction implementation
 		if ((*particles)[i].pos.y <= -10) {
 			(*particles)[i].velocity.z *= 0.4;
 			(*particles)[i].velocity.x *= 0.4;
