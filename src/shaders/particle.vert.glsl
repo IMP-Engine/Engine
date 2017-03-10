98b093dd004bb1a4ab9b06bd0c791da11c726e0d
artog <adam.ingmansson@gmail.com>
2017-03-10 18:02:20 +0100

    Shading problem fixed. Collisions is not working.

--- src/shaders/particle.vert.glsl
+++ src/shaders/particle.vert.glsl
@@ -7,16 +7,15 @@ uniform mat4 modelViewMatrix;
 uniform mat4 modelViewProjectionMatrix;
 uniform mat4 projectionMatrix;
 
-out vec3 color;
+flat out vec3 color;
 flat out vec3 centerPos;
 flat out float particleSize;
 
 void main() {
-	particleSize = 150;
+    particleSize = 150;
     gl_PointSize = particleSize / length(vec3(modelViewMatrix * vec4(position, 1.0)));
-	gl_Position = modelViewProjectionMatrix * vec4(position, 1.0);
+    gl_Position = modelViewProjectionMatrix * vec4(position, 1.0);
     color = vec3(0.89, 0.09, 0.05);
 
-
-	centerPos = (modelViewMatrix * vec4(position, 1.0)).xyz;
+    centerPos = (modelViewMatrix * vec4(position, 1.0)).xyz;
 }
