b90582a85f8b3a147f1cab7078110c0aab2fb287
Patrick Andersson <andersson.patrick95@gmail.com>
2017-05-26 20:22:57 +0200

    Data is being uploaded to GPU and can be read properly

--- src/GPU/GPU.cpp
+++ src/GPU/GPU.cpp
@@ -2,15 +2,74 @@
 
 GPU::GPU()
 {
+    glGenFramebuffers(1, &fbo);
 
+    glGenTextures(1, &posMain);
+    glGenTextures(1, &posSub);
+    glGenTextures(1, &invmasses);
+    glGenTextures(1, &constraint);
 }
 
 GPU::~GPU()
 {
+    glDeleteFramebuffers(1, &fbo);
+    glDeleteProgram(program);
+    glDeleteTextures(1, &posMain);
+    glDeleteTextures(1, &posSub);
+    glDeleteTextures(1, &invmasses);
+    glDeleteTextures(1, &constraint);
+}
 
+void initiateTexture(float width, float height, GLint internalFormat, GLenum format, void* data) {
+
+    glTexParameteri(target, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(target, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(target, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+    glTexImage2D(target, 0, internalFormat, width, height, 0, format, GL_FLOAT, data);
+}
+
+void GPU::restart(ParticleData & particles, ConstraintData & constraints)
+{
+    // Particle data
+    glBindTexture(target, posMain);
+    initiateTexture(10, 10, GL_RGB32F, GL_RGB, &particles.pPosition[0]);
+    glBindTexture(target, posSub);
+    initiateTexture(10, 10, GL_RGB32F, GL_RGB, NULL);
+    glBindTexture(target, invmasses);
+    initiateTexture(10, 10, GL_R32F, GL_RED, &particles.invmass[0]);
+
+    glBindBuffer(target, constraint);
+    float* data = (float*)malloc(constraints.distanceConstraints.cardinality * sizeof(glm::vec4));
+    for (int i = 0; i < constraints.distanceConstraints.cardinality; i += 4)
+    {
+        data[i] = (float) constraints.distanceConstraints.particles[i].x;     // eps ?
+        data[i + 1] = (float) constraints.distanceConstraints.particles[i].y; // eps ?
+        data[i + 2] = constraints.distanceConstraints.distance[i];
+        data[i + 3] = constraints.distanceConstraints.stiffness[i];
+    }
+    initiateTexture(100, 100, GL_RGBA32F, GL_RGBA, data);
+    free(data);
 }
 
 void GPU::run(ParticleData & particles, ConstraintData & constraints)
 {
+    //DEBUG
+    float* res = (float*)malloc(10 * 10 * sizeof(glm::vec3));
+    memset(res, 1, 10 * 10 * sizeof(glm::vec3));
+    
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
 
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, target, posMain, 0);
+
+    glReadBuffer(GL_COLOR_ATTACHMENT0);
+    glReadPixels(0, 0, 10, 10, GL_RGB, GL_FLOAT, res);
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    std::swap(posMain, posSub);
+
+    // DEBUG
+    free(res);
 }
--- src/GPU/GPU.h
+++ src/GPU/GPU.h
@@ -1,9 +1,22 @@
 #pragma once
 
+#ifndef __gl_h_
+#include "../glad/glad.h"
+#endif // !__gl_h_
+
 #include "../particles/ParticleData.h"
 #include "../constraints/ConstraintData.h"
 
 
+#define target GL_TEXTURE_2D
+
+/***************************************************************/
+/* For the verlet integration step, only position and velocity */
+/* is required in order to do the computation. It is therefore */
+/* unnecessary to store AoS. The constraint information, on    */
+/* the other hand is always queried all at the same time.      */
+/* Thusly, particle info is SoA. Constraint data is AoS        */
+/***************************************************************/
 
 class GPU
 {
@@ -12,8 +25,30 @@ public:
     GPU();
     ~GPU();
 
+    void restart(ParticleData & particles, ConstraintData & constraints);
+
     void run(ParticleData & particles, ConstraintData & constraints);
 
 private:
    
+    GLuint fbo;
+
+    /************************************/
+    /* +----------+----------+--------+ */
+    /* | variable | internal | format | */
+    /* +----------+----------+--------+ */
+    /* | posMain  | RGB32F   | RGB    | */
+    /* | posSub   | RGB32F   | RGB    | */
+    /* | invmass  | R32F     | RED    | */
+    /* +----------+----------+--------+ */
+    /************************************/
+    GLuint posMain, posSub, invmasses;          
+
+    /**************************************************/
+    /*         Distance constraint parameters         */
+    /* This represents a RGBA32F texture, i.e. vec4.  */
+    /* vec4.x and vec4.y is affected particle.        */
+    /* vec4.z is rest length. vec4.a is the stiffnes. */
+    /**************************************************/
+    GLuint constraint; // TODO is particles needed? Use indirection matrix
 };
